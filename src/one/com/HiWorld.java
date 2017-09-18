c2c689b0829d96e551856479d55b0f2c4d2e5c0d
ohyes007 <y6225474@163.com>
2017-09-19 07:07:32 +0800

    今天星期二

--- src/one/com/HiWorld.java
+++ src/one/com/HiWorld.java
@@ -7,8 +7,8 @@ package one.com;
 public class HiWorld {
     public static void main (String[] args) {
 Post post=new Post();
-post.title="杨知锜Github博客";
-        post.content="这是我的第一篇博客";
+post.title="杨知锜第二次Github博客";
+        post.content="这是我的第二篇博客";
         post.print();
 
 
